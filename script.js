b6eca37d45ac19a9f35b91d3711a712c0fbb68ac
Vladais <129493263+Vladais@users.noreply.github.com>
2023-04-25 22:59:26 +0200

    FE Homework5

---
+++ script.js
@@ -0,0 +1,40 @@
+ 
+// const number = prompt("Введите число")
+//  console.log(number * 0.1)
+// 
+// /*
+//  const number1 = number * 0.1
+//  console.log(number1)
+//  */
+
+//////////////////////////////////////////
+// const number1 = prompt("Введите число")
+// const number2 = prompt("Введите число")
+// if(number1 < number2) {
+//     console.log(number1)
+// }
+// else if(number2 < number1) {
+//     console.log(number2)
+// }
+
+//////////////////////////////////////////
+// const number = prompt("Введите число")
+// if(number > 100) {
+//     console.log("Число болше 100")
+// }  
+// else if(number == 100) {
+//     console.log("Число равно 100")
+// }
+// else {
+//     console.log("Число меньше 100")
+// } 
+
+////////////////////////////////////////////
+// const userName = prompt("Как вас зовут?")
+// const userAge = prompt("Сколько Вам лет?")
+// if(userAge >= 18) {
+//     console.log("Hello, " + userName)
+// }  
+// else {
+//     console.log("Hi, " + userName)
+// }
